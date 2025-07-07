5c2411ae6466d62ca4f5ea5eb5c1303496cba0da
Egor Antonov <eyemax@inbox.ru>
2025-07-07 19:04:56 +0300

    6.6.3 `0.5.0` compatibility

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,7 +14,7 @@ export const SBG = {
   OutboundLinksLimit: 30,
   DefaultCloseButtonText: '[x]',
   VersionHeader: 'Sbg-Version',
-  CompatibleVersion: '0.4.4',
+  CompatibleVersion: '0.5.0',
   Settings: 'settings',
   DefaultLang: 'en',
   GooglePhoto: 'https://lh3.googleusercontent.com/'
